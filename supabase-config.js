3f1e0b979b8acd53f4b145015f76c6936ff40b23
kozoshfe <kozoshfe@gmail.com>
2026-05-22 16:40:22 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-window.TRACKER_SUPABASE = {
-  url: 'https://qzcapeempzzdhicsweqz.supabase.co',
-  anonKey: 'sb_publishable_nXxnpG6C_RO9mVqcYEt1mg_Z9Z-dpDr',
-  table: 'tracker_state',
-  rowId: 'main',
-};
